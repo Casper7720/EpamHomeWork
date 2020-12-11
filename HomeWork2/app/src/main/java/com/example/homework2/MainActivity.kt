908595b7a871f1699b2ca393c32e503c3a9e97da
Hic <Kirill.russu.98@mail.ru>
2020-12-11 23:05:58 +0300

    Добавил Framelayout и изменение картинки при нажатии

--- HomeWork2/app/src/main/java/com/example/homework2/MainActivity.kt
+++ HomeWork2/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.homework2
 
 import android.icu.text.SimpleDateFormat
+import android.media.Image
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editorText : EditText
     private lateinit var buttonCreator: Button
     private lateinit var time : TextClock
+    private lateinit var imgKakasgi : ImageView
+    private lateinit var imgItachi : ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +31,22 @@ class MainActivity : AppCompatActivity() {
         editorText = findViewById(R.id.editTextTitle)
         buttonCreator = findViewById(R.id.buttonCreator)
         time = findViewById(R.id.textClock)
+        imgKakasgi = findViewById(R.id.imageView1)
+        imgItachi = findViewById(R.id.imageView2)
 
+        imgKakasgi.setOnClickListener {
+            imgItachi.visibility = View.VISIBLE
+            imgKakasgi.visibility = View.GONE
+        }
 
+        imgItachi.setOnClickListener {
+            imgItachi.visibility = View.GONE
+            imgKakasgi.visibility = View.VISIBLE
+        }
 
 
         buttonCreator.setOnClickListener {
-            if(!editorText.text.isEmpty()){
+            if(editorText.text.isNotEmpty()){
                 titleText.text = editorText.text
                 editorText.text = null
             }
@@ -43,18 +60,32 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
 
-        outState?.run {
+        outState.run {
             putString("Title", titleText.text.toString())
+            if(imgItachi.isVisible){
+                putString("Image", "Itachi")
+            }
+
         }
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        titleText.text = savedInstanceState?.getString("Title")
+        titleText.text = savedInstanceState.getString("Title")
+        if(savedInstanceState.get("Image") == "Itachi"){
+            imgItachi.visibility = View.VISIBLE
+            imgKakasgi.visibility = View.GONE
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 
 }
+
+
 
 
 
